c5e8da294eb59b65eb15a116449f49ffc02371f7
Anthony Weston <anthonyelliotweston@gmail.com>
2017-09-16 11:26:41 -0400

    partial db interaction

--- cockroach.py
+++ cockroach.py
@@ -1,28 +1,43 @@
 # Import the driver.
 import psycopg2
 
-# Connect to the "bank" database.
-conn = psycopg2.connect(database='bank', user='maxroach', host='localhost', port=26257)
+def create_table(database, user, host, port):
+	# Connect to the "bank" database.
+	conn = psycopg2.connect(database= 'doppelganger', user= 'test', host= 'localhost', port= 26257)
 
-# Make each statement commit immediately.
-conn.set_session(autocommit=True)
+	# Make each statement commit immediately.
+	conn.set_session(autocommit=True)
 
-# Open a cursor to perform database operations.
-cur = conn.cursor()
+	# Open a cursor to perform database operations.
+	cur = conn.cursor()
+	# Create the "accounts" table.
+	cur.execute("CREATE TABLE IF NOT EXISTS doppelganger.images (url STRING, hash STRING PRIMARY KEY)")
+	# Close the database connection.
+	cur.close()
+	conn.close()
 
-# Create the "accounts" table.
-cur.execute("CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT)")
 
-# Insert two rows into the "accounts" table.
-cur.execute("INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250)")
+def add_to_db(database_, user_, host_, port_, url_, hash_):
+	# Connect to the "bank" database.
+	conn = psycopg2.connect(database=database_, user=user_, host=host_, port=port_)
 
-# Print out the balances.
-cur.execute("SELECT id, balance FROM accounts")
-rows = cur.fetchall()
-print('Initial balances:')
-for row in rows:
-    print([str(cell) for cell in row])
+	# Make each statement commit immediately.
+	conn.set_session(autocommit=True)
 
-# Close the database connection.
-cur.close()
-conn.close()
+	# Open a cursor to perform database operations.
+	cur = conn.cursor()
+
+	# Insert two rows into the "accounts" table.
+	cur.execute("INSERT INTO images (url, hash) VALUES('random', %s)" %hash_)
+
+	# Close the database connection.
+	cur.close()
+	conn.close()
+
+def find_match_from_db():
+	return True
+
+if __name__ == "__main__":
+	create_table('doppelganger', 'test', 'localhost', 26257)
+
+	add_to_db('doppelganger', 'test', 'localhost', 26257, "random", "010010101")
