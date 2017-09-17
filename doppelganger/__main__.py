dba4e38c1bab900f4f63201ac46c00edeb967c03
Alex Rutar <alexrutarhome@gmail.com>
2017-09-16 20:50:31 -0600

    working getRep function to map image to representation

--- doppelganger/__main__.py
+++ doppelganger/__main__.py
@@ -1,5 +1,6 @@
 import openface
-import repr
+from repr import *
 
 if __name__ == "__main__":
     print("Imports work at least.")
+    print(getRep("face.jpeg"))
--- doppelganger/repr.py
+++ doppelganger/repr.py
@@ -1,6 +1,17 @@
 import openface
-align = openface.AlignDlib(args.dlibFacePredictor)
+import cv2
+import os
+
 def getRep(imgPath):
+    # some initial variable setup (!)
+    # TODO figure out what this is doing
+    modelDir = '/root/openface/models/'
+    dlibModelDir = os.path.join(modelDir, 'dlib')
+    align = openface.AlignDlib(os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
+    imgDim = 96
+    openfaceModelDir = os.path.join(modelDir, 'openface')
+    net = openface.TorchNeuralNet(os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'), imgDim)
+
     # load some image
     bgrImg = cv2.imread(imgPath)
 
@@ -19,7 +30,7 @@ def getRep(imgPath):
         return None
 
     # build an aligned face
-    alignedFace = align.align(args.imgDim, rgbImg, bb,
+    alignedFace = align.align(imgDim, rgbImg, bb,
                               landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
 
     # could not align face properly
