2f11d1a0a3280f028592151798732621454774bb
Jeffrey Zhao <jezhao1999@gmail.com>
2017-09-17 08:19:19 -0400

    bugfixes

--- app/apps/main/views.py
+++ app/apps/main/views.py
@@ -27,40 +27,48 @@ def get_top_3(fname):
     idb.load_from_file("test_save")
 
     mat = getRep(fname)
+    if mat is None:
+        throw Exception("couldn't find face")
     dists, names = idb.k_nearest(mat, 3)
     return dists, names
 
 def handle_post(self, img_bytes):
     user_id = get_new_user_id()
     write_bytes_to_file(img_bytes, user_id)
-    self.write('/gallery/%d' % user_id)
+    return '/gallery/%d' % user_id
 
 class HomeHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('home.html')
+    def get(self, face_error=False):
+        self.render('home.html', face_error=face_error)
 
 class UploadHandler(tornado.web.RequestHandler):
     def post(self):
-        handle_post(self, self.request.files['photo'][0]['body'])
+        self.redirect(handle_post(self, self.request.files['photo'][0]['body']))
 
 class WebcamHandler(tornado.web.RequestHandler):
     def post(self):
-        handle_post(self, self.request.files['webcam'][0]['body'])
+        self.write(handle_post(self, self.request.files['webcam'][0]['body']))
 
 class GalleryHandler(tornado.web.RequestHandler):
     def get(self, user_id):
         img_file = get_img_by_user_id(user_id)
-        x = get_top_3('/root/doppelganger/app/static/img/users/' + img_file)
-        paintings = []
-        for i in range(3):
-            paintings.append((x[0][i], x[1][i]))
-        paintings.sort()
-        with Image.open('./static/img/users/%s' % img_file) as user_img:
-            self.render(
-                'gallery.html',
-                user_id = user_id,
-                user_img_file = img_file,
-                user_img_width = user_img.width,
-                user_img_height = user_img.height,
-                paintings = paintings
+        try:
+            x = get_top_3('/root/doppelganger/app/static/img/users/' + img_file)
+            paintings = []
+            for i in range(3):
+                paintings.append((x[0][i], x[1][i]))
+            paintings.sort()
+            with Image.open('./static/img/users/%s' % img_file) as user_img:
+                self.render(
+                    'gallery.html',
+                    user_id = user_id,
+                    user_img_file = img_file,
+                    user_img_width = user_img.width,
+                    user_img_height = user_img.height,
+                    paintings = paintings
+                )
+        except:
+            self.redirect(
+                '/',
+                face_error=True
             )
